7d3f2d19a28a9abc2173ee4d4992378e0a5be99d
planetsputter <>
2026-08-23 20:52:04 -0500

    test.sh: Introduce stasm_accept() and stasm_reject(), quote test names

--- test.sh
+++ test.sh
@@ -61,63 +61,77 @@ STASM="$MEMPROF ../stasm/bin/stasm"
 STEM="$MEMPROF ../stem/bin/stem"
 
 # Run unit test executables
-test_begin testing smap
+test_begin "testing smap"
 ../util/test/smaptest
-test_begin testing emulated memory
+test_begin "testing emulated memory"
 ../stem/test/memtest
-test_begin testing utf8 library
+test_begin "testing utf8 library"
 ../util/test/utf8test
-test_begin testing literal parsing
+test_begin "testing literal parsing"
 ../util/test/littest
-test_begin testing expression parsing
+test_begin "testing expression parsing"
 ../stasm/test/exprtest 2>/dev/null
 
 # Assemble a file which contains every opcode
-test_begin assembling all opcodes
+test_begin "assembling all opcodes"
 $STASM allops.sta
-test_begin disassembling all opcodes
+test_begin "disassembling all opcodes"
 $DISTASM a.stb -o dis.sta
-test_begin checking for symmetric dis/assembly
+test_begin "checking for symmetric dis/assembly"
 $STASM dis.sta -o b.stb
 cmp a.stb b.stb
 
 # Check assembly of pseudo-ops
-test_begin assembling pseudo-ops
+test_begin "assembling pseudo-ops"
 $STASM psops.sta
-test_begin disassembling pseudo-ops
+test_begin "disassembling pseudo-ops"
 $DISTASM a.stb -o dis.sta
-test_begin checking for proper disassembly
+test_begin "checking for proper disassembly"
 cmp psops-dis.sta dis.sta
-test_begin checking compaction of pseudo-ops
+test_begin "checking compaction of pseudo-ops"
 $STASM compact.sta
 $DISTASM a.stb --addr -o dis.sta
 cmp compact-dis.sta dis.sta
 
 # Check various errors are detected by the assembler
-test_begin testing rejection of opcode outside of section
+stasm_accept() {
+	$STASM <<EOF
+section 0x3000
+$*
+EOF
+}
+stasm_reject() {
+	if $STASM <<EOF 2>/dev/null
+section 0x3000
+$*
+EOF
+	then false; fi
+}
+
+test_begin "testing rejection of opcode outside of section"
 if $STASM <<EOF 2>/dev/null
 push64 0
 EOF
 then false; fi
 
-test_begin testing rejection of label outside of section
+test_begin "testing rejection of label outside of section"
 if $STASM <<EOF 2>/dev/null
 :test_label
 EOF
 then false; fi
 
-test_begin testing rejection of strings outside of section
+test_begin "testing rejection of strings outside of section"
 if $STASM <<EOF 2>/dev/null
 strings
 EOF
 then false; fi
 
-test_begin testing rejection of invalid maximum number of sections
+test_begin "testing rejection of invalid maximum number of sections"
 if $STASM --maxnsec 0 </dev/null 2>/dev/null; then false; fi
 if $STASM --maxnsec -1 </dev/null 2>/dev/null; then false; fi
 $STASM --maxnsec 1 </dev/null
 
-test_begin testing rejection of invalid section address
+test_begin "testing rejection of invalid section address"
 if $STASM <<EOF 2>/dev/null
 section -1
 EOF
@@ -127,141 +141,74 @@ section a
 EOF
 then false; fi
 
-test_begin testing rejection of empty symbol name
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-push64 \$
-EOF
-then false; fi
+test_begin "testing rejection of empty symbol name"
+stasm_reject 'push64 $'
+stasm_accept 'define a 0; push64 $a'
 
-test_begin testing rejection of integer literal symbol name
-if $STASM <<EOF 2>/dev/null
-define 1 0
-EOF
-then false; fi
+test_begin "testing rejection of integer literal symbol name"
+stasm_reject 'define 1 0'
 
-test_begin testing rejection of string literal symbol name
-if $STASM <<EOF 2>/dev/null
-define "a" 1
-EOF
-then false; fi
-$STASM <<EOF
-define a "1"
-EOF
+test_begin "testing rejection of string literal symbol name"
+stasm_reject 'define "a" 1'
+stasm_accept 'define a 1'
 
-test_begin testing rejection of label symbol name
-if $STASM <<EOF 2>/dev/null
-define :test1 :test2
-EOF
-then false; fi
-$STASM <<EOF
-define test1 :test1
-EOF
+test_begin "testing rejection of label symbol name"
+stasm_reject 'define :test1 :test2'
+stasm_accept 'define test1 :test2'
 
-test_begin testing rejection of undefined symbol
-if $STASM <<EOF 2>/dev/null
-define a 1
-section 0x3000
-push64 \$b
-EOF
-then false; fi
+test_begin "testing rejection of undefined symbol"
+stasm_reject 'push64 $b'
 
-test_begin testing rejection of empty label name
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-:
-EOF
-then false; fi
+test_begin "testing rejection of empty label name"
+stasm_reject ':'
 
-test_begin testing rejection of duplicate labels
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-:test
-:test
-EOF
-then false; fi
+test_begin "testing rejection of duplicate labels"
+stasm_reject ':test1; :test1'
+stasm_accept ':test1; :test2'
 
-test_begin testing rejection of unquoted include
-if $STASM <<EOF 2>/dev/null
-include psops.sta
-EOF
-then false; fi
+test_begin "testing rejection of unquoted include"
+stasm_reject 'include psops.sta'
 
-test_begin testing rejection of invalid opcode
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-test
-EOF
-then false; fi
+test_begin "testing rejection of invalid opcode"
+stasm_reject 'test'
 
-test_begin testing rejection of incomplete statement
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-push64
-EOF
-then false; fi
+test_begin "testing rejection of incomplete statement"
+stasm_reject 'push64'
 
-test_begin testing rejection of unexpected expression
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-add8 0
-EOF
-then false; fi
+test_begin "testing rejection of unexpected expression"
+stasm_reject 'add8 0'
 
-test_begin testing rejection of out-of-range arguments
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-push8asu16 -1
-EOF
-then false; fi
+test_begin "testing rejection of out-of-range arguments"
+stasm_reject 'push8as8 -129'
+stasm_accept 'push8as8 -128'
+stasm_accept 'push8as8 255'
+stasm_reject 'push8as8 256'
 
-test_begin testing rejection of disallowed bracket notation
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-push8as8 [0]
-EOF
-then false; fi
+test_begin "testing rejection of disallowed bracket notation"
+stasm_reject 'push8as8 [0]'
+stasm_accept 'push8 [0]'
 
-test_begin testing detection of missing bracket notation
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-pop8 0
-EOF
-then false; fi
+test_begin "testing detection of missing bracket notation"
+stasm_reject 'pop8 0'
+stasm_accept 'pop8 [0]'
 
-test_begin testing rejection of improper SFP notation
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-push64 [0+SFP]
-EOF
-then false; fi
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-push64 [+SFP]
-EOF
-then false; fi
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-push64 [0-SFP]
-EOF
-then false; fi
-if $STASM <<EOF 2>/dev/null
-section 0x3000
-push64 [-SFP]
-EOF
-then false; fi
+test_begin "testing rejection of improper SFP notation"
+stasm_reject 'push64 [0+SFP]'
+stasm_reject 'push64 [+SFP]'
+stasm_reject 'push64 [0-SFP]'
+stasm_reject 'push64 [-SFP]'
 
 # Run individual tests
-test_begin testing add, sub
+test_begin "testing add, sub"
 $STASM test-add-sub.sta
 $STEM a.stb
-test_begin testing mul, div, mod
+test_begin "testing mul, div, mod"
 $STASM test-mul-div-mod.sta
 $STEM a.stb
-test_begin testing bitwise logical operations
+test_begin "testing bitwise logical operations"
 $STASM test-bit-ops.sta
 $STEM a.stb
-test_begin testing interrupts
+test_begin "testing interrupts"
 $STASM test-int.sta
 $STEM a.stb
 
